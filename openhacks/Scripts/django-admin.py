b4f4db58b9a68e139290f3b53bdcca2758f3ab9b
SeungTaeKim <equus3144@gmail.com>
2019-06-27 00:56:02 +0900

    openhack default setting

---
+++ openhacks/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\equus\desktop\openhacks\openhackwi-fi\openhacks\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

689bb8d93389f0292fb15387aee44bbe508f12c9
SeungTaeKim <equus3144@gmail.com>
2019-06-27 10:05:17 +0900

    settings.py mysql setting

--- openhacks/wico/wico/settings.py
+++ openhacks/wico/wico/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import pymysql
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -78,15 +79,15 @@ WSGI_APPLICATION = 'wico.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
      'external': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
+        'NAME': 'openhack',
+        'USER': 'openhack',
+        'PASSWORD': 'wifi',
+        'HOST': '10.10.4.102',
     }
 }
 

ac9b9486e6c9ffc8c8dca43bedb21d281cdf0813
DESKTOP-ET07CGU\Junsik Choi <2318ws@naver.com>
2019-06-27 16:49:55 +0900

    Mac 날짜별 체류시간

--- openhacks/wico/wifi/urls.py
+++ openhacks/wico/wifi/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('',views.main,name='main'),
     path('maclist/',views.macList, name='macList'),
     path('date/',views.getDate, name='date'),
+    path('machour/',views.getMacHour, name='machour'),
 
 ]
